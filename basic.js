4497eca18628a8e11667cae24a184e509ee665ce
TestWithVivek <vivekvardhan2706@gmail.com>
2025-08-24 11:23:11 +0530

    this is js file

---
+++ basic.js
@@ -0,0 +1,25 @@
+console.log("Hanuman")
+console.log("THis is my first basic standared program");
+
+let a = 10;
+let b = 20;
+let c = a + b;  
+console.log("The sum of a and b is: " + c);
+
+console.log(typeof a); // Output: number
+console.log(typeof b); // Output: number
+
+let s = "Hanuman"
+console.log(typeof s); // Output: string
+console.log("The length of the string is: " + s.length); // Output:
+
+let f = true;
+
+console.log(typeof f);
+
+let n = "20"
+console.log(typeof n); // Output: undefined
+
+let t = true;
+console.log(!t);
+
